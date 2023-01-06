d25c53551ec31bd5f906196030a48dd470617560
slydesigns <sylvestorsielei@gmail.com>
2023-01-06 16:15:37 +0300

    cafe management system

---
+++ src/main/java/com/inn/cafe/CafeManagementSysyemApplication.java
@@ -0,0 +1,13 @@
+package com.inn.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeManagementSysyemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeManagementSysyemApplication.class, args);
+	}
+
+}
