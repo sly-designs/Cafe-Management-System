eabe09b7738f23280796b81dd40f1e908cea4bb7
slydesigns <sylvestorsielei@gmail.com>
2023-01-16 21:47:04 +0300

    security config

--- src/main/java/com/inn/cafe/JWT/JwtUtil.java
+++ src/main/java/com/inn/cafe/JWT/JwtUtil.java
@@ -51,7 +51,7 @@ public class JwtUtil {
     }
     public Boolean validateToken(String token, UserDetails userDetails){
         final String username = extractUsername(token);
-        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));                                                                                                                                                                                                                                                                                                                                                                                                 );
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
 
     }
 
---
+++ src/main/java/com/inn/cafe/JWT/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.inn.cafe.JWT;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig implements WebSecurityConfigurerAdapter{
+
+}
